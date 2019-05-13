19149a27044c6db5599642619898ef3ff8cad2a5
knishimae <49812029+knishimae@users.noreply.github.com>
2019-05-13 23:21:52 +0900

    Add files via upload

---
+++ program/4.MINIST_Numpy_CNN_simple.py
@@ -0,0 +1,69 @@
+﻿import sys, os
+sys.path.append(os.pardir)
+import pickle
+import numpy as np
+from collections import OrderedDict
+from common import layers
+from common import optimizer
+from data.mnist import load_mnist
+import matplotlib.pyplot as plt
+from common import optimizer
+
+
+# データの読み込み
+(x_train, d_train), (x_test, d_test) = load_mnist(flatten=False)
+
+print("データ読み込み完了")
+
+# 処理に時間のかかる場合はデータを削減 
+x_train, d_train = x_train[:5000], d_train[:5000]
+x_test, d_test = x_test[:1000], d_test[:1000]
+
+#CNN(sinmple)を設定(layers.pyにクラス定義されている)
+network = layers.SimpleConvNet(input_dim=(1,28,28), conv_param = {'filter_num': 30, 'filter_size': 5, 'pad': 0, 'stride': 1},
+                        hidden_size=100, output_size=10, weight_init_std=0.01)
+
+optimizer = optimizer.Adam()
+
+iters_num = 1000
+train_size = x_train.shape[0]
+batch_size = 100
+
+train_loss_list = []
+accuracies_train = []
+accuracies_test = []
+
+plot_interval=10
+
+
+
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    d_batch = d_train[batch_mask]
+    
+    grad = network.gradient(x_batch, d_batch)
+    optimizer.update(network.params, grad)
+
+    loss = network.loss(x_batch, d_batch)
+    train_loss_list.append(loss)
+
+    if (i+1) % plot_interval == 0:
+        accr_train = network.accuracy(x_train, d_train)
+        accr_test = network.accuracy(x_test, d_test)
+        accuracies_train.append(accr_train)
+        accuracies_test.append(accr_test)
+        
+        print('Generation: ' + str(i+1) + '. 正答率(トレーニング) = ' + str(accr_train))
+        print('                : ' + str(i+1) + '. 正答率(テスト) = ' + str(accr_test))               
+
+lists = range(0, iters_num, plot_interval)
+plt.plot(lists, accuracies_train, label="training set")
+plt.plot(lists, accuracies_test,  label="test set")
+plt.legend(loc="lower right")
+plt.title("accuracy")
+plt.xlabel("count")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+# グラフの表示
+plt.show()
